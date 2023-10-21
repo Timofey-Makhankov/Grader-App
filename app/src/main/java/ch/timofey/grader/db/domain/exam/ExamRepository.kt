c7dd0224312ae8484654bd209767f9e7ab70aa26
Timofey-Makhankov <timofey.makhankov@noseryoung.com>
2023-10-21 17:42:50 +0200

    updated Exam domain
    added KDoc and logging to the Exam Domain in Repository interface and implementation

--- app/src/main/java/ch/timofey/grader/db/domain/exam/ExamRepository.kt
+++ app/src/main/java/ch/timofey/grader/db/domain/exam/ExamRepository.kt
@@ -4,13 +4,87 @@ import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
 interface ExamRepository {
+    /**
+     * save Exam Entity
+     *
+     * @param exam entity
+     * @see Exam
+     */
     suspend fun saveExam(exam: Exam)
-    suspend fun deleteExam(exam: Exam)
+
+    /**
+     * delete exam entity by id
+     *
+     * @param id [UUID]
+     * @see Exam
+     */
+    suspend fun deleteExamById(id: UUID)
+
+    /**
+     * update Exam entity
+     *
+     * @param exam
+     * @see Exam
+     */
     suspend fun updateExam(exam: Exam)
+
+    /**
+     * get Exam entity by id
+     *
+     * @param id [UUID]
+     * @return Optional Exam Entity
+     */
     suspend fun getExamById(id: UUID): Exam?
+
+    /**
+     * update isSelected field in exam entity by id
+     *
+     * @param id [UUID]
+     * @param value boolean
+     * @see Exam
+     */
     suspend fun updateIsSelectedExam(id: UUID, value: Boolean)
+
+    /**
+     * update onDelete field in division entity by id
+     *
+     * @param id [UUID]
+     * @param value boolean
+     * @see Exam
+     */
     suspend fun updateOnDelete(id: UUID, value: Boolean)
+
+    /**
+     * update average grade for parent module entity by id
+     *
+     * @param id module [UUID]
+     * @param value Double
+     * @see Exam
+     */
     suspend fun updateModuleGradeById(id: UUID, value: Double)
-    fun getAllExams(): Flow<List<Exam>>
+
+    /**
+     * get a flow list of exams
+     *
+     * @return flow list of exams or empty flow list
+     * @see Exam
+     */
+    fun getAllExamsFlow(): Flow<List<Exam>>
+
+    /**
+     * get list of exams
+     *
+     * @return list of exams or empty list
+     * @see Exam
+     */
+    fun getAllExams(): List<Exam>
+
+    /**
+     * get flow list of exams by parent module id
+     *
+     * @param id module [UUID]
+     * @return flow list of exams or empty flow list
+     * @see Exam
+     */
     fun getAllExamsFromModuleId(id: UUID): Flow<List<Exam>>
 }
--- app/src/main/java/ch/timofey/grader/db/domain/exam/ExamRepositoryImpl.kt
+++ app/src/main/java/ch/timofey/grader/db/domain/exam/ExamRepositoryImpl.kt
@@ -1,42 +1,72 @@
 package ch.timofey.grader.db.domain.exam
 
+import android.util.Log
 import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
 class ExamRepositoryImpl(private val examDao: ExamDao) : ExamRepository {
+    private val tag: String = "ExamRepositoryImpl"
     override suspend fun saveExam(exam: Exam) {
+        Log.v(tag, "trying to save exam")
         examDao.save(exam)
+        Log.d(tag, "exam entity saved")
     }
 
-    override suspend fun deleteExam(exam: Exam) {
-        examDao.delete(exam)
+    override suspend fun deleteExamById(id: UUID) {
+        Log.v(tag, "trying to delete exam by id: $id")
+        examDao.deleteById(id)
+        Log.d(tag, "exam entity deleted")
     }
 
     override suspend fun updateExam(exam: Exam) {
+        Log.v(tag, "trying to update exam")
         examDao.update(exam)
+        Log.d(tag, "exam entity updated")
     }
 
     override suspend fun getExamById(id: UUID): Exam? {
-        return examDao.getById(id)
+        Log.v(tag, "trying to find exam entity by id: $id")
+        val result = examDao.getById(id)
+        Log.d(tag, "Optional Exam found")
+        return result
     }
 
     override suspend fun updateIsSelectedExam(id: UUID, value: Boolean) {
+        Log.v(tag, "trying to update isSelected on Exam by id: $id and value: $value")
         examDao.updateIsSelected(id, value)
+        Log.d(tag, "isSelected on exam updated")
     }
 
     override suspend fun updateOnDelete(id: UUID, value: Boolean) {
+        Log.v(tag, "trying to update onDelete on Exam by id: $id and value: $value")
         examDao.updateOnDelete(id, value)
+        Log.d(tag, "onDelete on exam updated")
     }
 
     override suspend fun updateModuleGradeById(id: UUID, value: Double) {
+        Log.v(tag, "trying to update average grade on parent module id: $id and grade: $value")
         examDao.updateModuleGradeById(id, value)
+        Log.d(tag, "average grade on module updated")
     }
 
-    override fun getAllExams(): Flow<List<Exam>> {
-        return examDao.getAll()
+    override fun getAllExamsFlow(): Flow<List<Exam>> {
+        Log.v(tag, "trying to get flow list of exams")
+        val result = examDao.getAllFlow()
+        Log.d(tag, "flow list exams found")
+        return result
+    }
+
+    override fun getAllExams(): List<Exam> {
+        Log.v(tag, "trying to get list of exams")
+        val result = examDao.getAll()
+        Log.d(tag, "list of exams found")
+        return result
     }
 
     override fun getAllExamsFromModuleId(id: UUID): Flow<List<Exam>> {
-        return examDao.getAllExamsFromModuleId(id)
+        Log.v(tag, "trying to get flow list of exams from module id: $id")
+        val result =  examDao.getAllExamsFromModuleId(id)
+        Log.d(tag, "flow list exams from module found")
+        return result
     }
 }
