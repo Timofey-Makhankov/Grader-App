1e6f39d3dfbfd157353147f8c19ced83c675298c
Timofey-Makhankov <timofey.makhankov@noseryoung.com>
2023-10-29 17:55:36 +0100

    fixed screen ui with switchtext

--- app/src/main/java/ch/timofey/grader/ui/screen/settings/SettingsScreen.kt
+++ app/src/main/java/ch/timofey/grader/ui/screen/settings/SettingsScreen.kt
@@ -1,42 +1,19 @@
 package ch.timofey.grader.ui.screen.settings
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.DrawerState
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.rememberDrawerState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ch.timofey.grader.db.AppTheme
 import ch.timofey.grader.navigation.Screen
-import ch.timofey.grader.ui.components.AppBar
-import ch.timofey.grader.ui.components.NavigationDrawer
+import ch.timofey.grader.ui.components.organisms.AppBar
+import ch.timofey.grader.ui.components.molecules.NavigationDrawer
+import ch.timofey.grader.ui.components.molecules.SwitchText
 import ch.timofey.grader.ui.theme.GraderTheme
 import ch.timofey.grader.ui.theme.spacing
 import ch.timofey.grader.ui.utils.UiEvent
@@ -127,40 +104,54 @@ fun SettingsScreen(
                         onDismissRequest = { expanded.value = false },
 
                         ) {
-                        AppTheme.entries.filter { value -> value != state.appTheme }.forEach { theme ->
-                            DropdownMenuItem(text = { Text(text = theme.title) }, onClick = {
-                                onEvent(SettingsEvent.OnThemeChange(theme))
-                                expanded.value = false
-                            })
-                        }
+                        AppTheme.entries.filter { value -> value != state.appTheme }
+                            .forEach { theme ->
+                                DropdownMenuItem(text = { Text(text = theme.title) }, onClick = {
+                                    onEvent(SettingsEvent.OnThemeChange(theme))
+                                    expanded.value = false
+                                })
+                            }
                     }
                 }
                 Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
-                Row(
+//                Row(
+//                    modifier = Modifier.padding(horizontal = MaterialTheme.spacing.medium),
+//                    horizontalArrangement = Arrangement.Center,
+//                    verticalAlignment = Alignment.CenterVertically
+//                ) {
+//                    Text(text = "calculate points")
+//                    Spacer(modifier = Modifier.weight(1f))
+//                    Switch(
+//                        checked = state.calculatePointsState,
+//                        onCheckedChange = { onEvent(SettingsEvent.OnCalculatePointsChange(it)) })
+//                }
+//                Row(
+//                    modifier = Modifier.padding(horizontal = MaterialTheme.spacing.medium),
+//                    horizontalArrangement = Arrangement.Center,
+//                    verticalAlignment = Alignment.CenterVertically
+//                ) {
+//                    Text(text = "double points")
+//                    Spacer(modifier = Modifier.weight(1f))
+//                    Switch(
+//                        checked = state.doublePointsState,
+//                        onCheckedChange = { onEvent(SettingsEvent.OnDoublePointsChange(it)) },
+//                        enabled = state.calculatePointsState
+//                    )
+//                }
+                SwitchText(
                     modifier = Modifier.padding(horizontal = MaterialTheme.spacing.medium),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "calculate points")
-                    Spacer(modifier = Modifier.weight(1f))
-                    Switch(
-                        checked = state.calculatePointsState,
-                        onCheckedChange = { onEvent(SettingsEvent.OnCalculatePointsChange(it)) })
-                }
-                Row(
+                    onValueChange = { value -> onEvent(SettingsEvent.OnCalculatePointsChange(value)) },
+                    value = state.calculatePointsState,
+                    name = "Calculate Points"
+                )
+                SwitchText(
                     modifier = Modifier.padding(horizontal = MaterialTheme.spacing.medium),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "double points")
-                    Spacer(modifier = Modifier.weight(1f))
-                    Switch(
-                        checked = state.doublePointsState,
-                        onCheckedChange = { onEvent(SettingsEvent.OnDoublePointsChange(it)) },
-                        enabled = state.calculatePointsState
-                    )
-                }
-
+                    onValueChange = { value -> onEvent(SettingsEvent.OnDoublePointsChange(value)) },
+                    value = state.doublePointsState,
+                    enabled = state.calculatePointsState,
+                    name = "Double Points",
+                    extraInfo = if (!state.calculatePointsState) "Enable 'Calculate Points' to enable setting" else ""
+                )
             }
         }
     }
