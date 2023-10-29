dfb81ea7680e04087f1df8471939c5a5c5388e7c
Timofey-Makhankov <timofey.makhankov@noseryoung.com>
2023-10-29 17:52:22 +0100

    updated Grade Input Field

--- app/src/main/java/ch/timofey/grader/ui/components/atom/GradeInputField.kt
+++ app/src/main/java/ch/timofey/grader/ui/components/atom/GradeInputField.kt
@@ -1,50 +1,53 @@
-package ch.timofey.grader.ui.components
+package ch.timofey.grader.ui.components.atom
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import ch.timofey.grader.ui.theme.GraderTheme
 import ch.timofey.grader.ui.theme.spacing
 import ch.timofey.grader.ui.utils.RegexPatterns
 
 @Composable
 fun GradeInputField(
     modifier: Modifier = Modifier,
-    onGradeChange: (value: Double) -> Unit,
-    onWeightChange: (value: Double) -> Unit
+    onGradeChange: (value: String) -> Unit,
+    onWeightChange: (value: String) -> Unit,
+    grade: String,
+    weight: String
 ) {
-    val grade = remember { mutableDoubleStateOf(0.0) }
-    val weight = remember { mutableDoubleStateOf(0.0) }
+    val localGrade = remember { mutableStateOf(grade) }
+    val localWeight = remember { mutableStateOf(weight) }
     Row(
         modifier = modifier
     ) {
-        TextField(
-            value = grade.doubleValue.toString(),
+        OutlinedTextField(
+            value = localGrade.value,
             onValueChange = {
-                if (it.isEmpty() || it.matches(RegexPatterns.OnlyNumberRegex)) {
-                    onGradeChange(it.toDouble())
-                }
+                localGrade.value = it
+                onGradeChange(it)
             },
             shape = MaterialTheme.shapes.medium,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
 
         )
         Spacer(modifier = modifier.width(MaterialTheme.spacing.medium))
-        TextField(
-            value = weight.doubleValue.toString(),
+        OutlinedTextField(
+            value = localWeight.value,
             onValueChange = {
-                if (it.isEmpty() || it.matches(RegexPatterns.OnlyNumberRegex)) {
-                    onWeightChange(it.toDouble())
-                }
+                localWeight.value = it
+                onWeightChange(it)
             },
             shape = MaterialTheme.shapes.medium,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
@@ -55,5 +58,7 @@ fun GradeInputField(
 @Preview(showBackground = true)
 @Composable
 private fun GradeInputFieldPreview() {
-    GradeInputField(onGradeChange = {}, onWeightChange = {})
+    GraderTheme {
+        GradeInputField(onGradeChange = {}, onWeightChange = {}, grade = "4.5", weight = "1.0")
+    }
 }
